6c2016b3ec52a96a6479f7aed2c04be60c733804
ryoff <ryoffes@gmail.com>
2015-04-30 19:14:58 +0900

    add validates

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,6 @@
 class Tweet < ActiveRecord::Base
+  validates :search_word, presence: true, length: { maximum:  100 }
+  validates :name,        presence: true, length: { maximum:  100 }
+  validates :full_text,   presence: true, length: { maximum:  500 }
+  validates :uri,         presence: true, length: { maximum: 1000 }
 end
